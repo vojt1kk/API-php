24c1f188832da0979078d557d118e2d70e314570
vojt1kk <kocourek.vojt1k@gmail.com>
2025-07-09 17:11:46 +0200

    commit

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,12 +1,25 @@
 <?php
+namespace App\Http\Controllers\Api;
+
 
 use App\Models\Category;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
  
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category) 
+    {
+        return new CategoryResource($category);
+    } 
+
+    public function list()
+    {
+        return CategoryResource::collection(Category::all());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ 
+Route::get('lists/categories', [\App\Http\Controllers\Api\CategoryController::class, 'list']);
+
+Route::get('categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']); 
+Route::get('categories/{category}', [\App\Http\Controllers\Api\CategoryController::class, 'show']); 
